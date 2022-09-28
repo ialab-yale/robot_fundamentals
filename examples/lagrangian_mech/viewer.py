8f244f76dbe4b8d0389330057e69037d4afcf149
ian <iabr4073@gmail.com>
2022-09-28 18:27:52 -0400

    adding examples

---
+++ examples/lagrangian_mech/viewer.py
@@ -0,0 +1,25 @@
+import meshcat
+from meshcat import Visualizer
+import meshcat.geometry as mc_geom
+import meshcat.transformations as mc_trans
+import numpy as np
+
+class BallViewer(Visualizer):
+    def __init__(self, radius=1.0) -> None:
+        Visualizer.__init__(self)
+        self._body = self["ball"]
+        self._body.set_object(
+            mc_geom.Sphere(radius),
+            mc_geom.MeshLambertMaterial(
+                    color=0x00ff00,
+                    opacity=0.5,
+                    wireframe=True,
+                    reflectivity=0.8)
+        )
+    def render(self, q):
+        x,z,th = q
+        tf = mc_trans.compose_matrix(
+                        translate=[x,0,z], 
+                        angles=[0.,th,0.]
+            )
+        self._body.set_transform(tf)
